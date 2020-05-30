0d1b776cda29bbfe21465b08327fa213794367ab
gxnas <52808122+gxnas@users.noreply.github.com>
2020-05-30 12:02:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
