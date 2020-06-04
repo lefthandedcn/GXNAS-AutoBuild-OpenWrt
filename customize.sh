39b960478319f7b462c69604b4aed535d3c8291b
gxnas <52808122+gxnas@users.noreply.github.com>
2020-06-04 11:10:50 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,4 +2,4 @@
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
-sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/opentomcat' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase=/luci-static/opentomcat' package/lean/default-settings/files/default-settings
