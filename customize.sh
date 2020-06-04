a013cb7abeea39feb79cdfc6f157f352099f66b0
gxnas <52808122+gxnas@users.noreply.github.com>
2020-06-04 12:34:31 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,5 +2,3 @@
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.1.11/g' package/base-files/files/bin/config_generate
 
-#取消bootstrap为默认主题
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
