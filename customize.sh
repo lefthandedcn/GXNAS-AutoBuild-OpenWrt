e0b36190541c0eb36b1e1c8d1ff997c9cc9065ae
gxnas <52808122+gxnas@users.noreply.github.com>
2020-12-14 08:00:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -3,4 +3,3 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #修改版本号
 #sed -i 's/OpenWrt/Bin AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
-echo "echo \"`date "+%Y-%m-%d %H:%M"` (commit:`git log -1 --format=format:'%C(bold white)%h%C(reset)'`)\" >> /etc/Compile_time" >> package/lean/default-settings/files/zzz-default-settings
